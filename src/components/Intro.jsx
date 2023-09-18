67920c792d335fa646d0b0f727b4e16ea80cf1c3
JNario31 <31johnnario@gmail.com>
2023-09-18 14:49:20 -0400

    Changes to intro.jsx added about text

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,25 +4,11 @@ function Intro() {
    return (
       <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
          <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">John Nario</h1>
-         <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
+         <p className="text-base md:text-xl mb-3 font-medium">Computer Engineering Student</p>
          <p className="text-sm max-w-xl mb-6 font-bold">
-            I'm an indie full-stack developer and content creator building my
-            version of the digital world one step at a time. All coding projects
-            are built from the ground up, from planning and designing all the way
-            to solving real-life problems with code.
+         Hello! I'm a passionate Computer Engineering student at York University. As I embark on my journey, I'm excited to find my first job within the industry and contribute my skills and knowledge to meaningful projects. 
             <br />
-            All video content is built the same way, from ideation and planning,
-            all the way to finalizing the content with artistic touches. I publish
-            that content on my YouTube channel{' '}
-            <a
-               href="https://youtube.com/fknight"
-               target="_blank"
-               className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-red-600"
-               rel="noreferrer noopener"
-            >
-               ForrestKnight
-            </a>{' '}
-            to more than 400k subscribers.
+            To showcase my work and skills, I've crafted this portfolio website using React.js, Tailwind CSS, and Vite. It serves as a digital canvas where I exhibit my projects, experiences, and interests in the field of computer engineering. I'm continually expanding my skill set, exploring new technologies, and seeking opportunities to collaborate with like-minded individuals and organizations.
          </p>
       </div>
    )
